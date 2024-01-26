6a694b3dfee95be4ae415b7296811f6fc6bd11af
Ion Urse <ionvurse@gmail.com>
2024-01-26 00:07:58 +0000

    defensive programming added to input

--- student_register.py
+++ student_register.py
@@ -1,20 +1,16 @@
 """A program that will store the student ID numbers of 
 a variable number of students specified by the program's user """
 
-# if no_of_students.isnumeric() is False:
-#     # print("How many students will sit in the exam?: ")
-#     no_of_students = input("""\tHow many students will sit in the exam?:
-#         Please enter a numeric value: """)
-# else:
-#     no_of_students = int(no_of_students)
-#     print(no_of_students)
-no_of_students = input("\tHow many students will sit in the exam?: ")
+
+no_of_students = input("How many students will sit in the exam?: ")
+while no_of_students.isnumeric() is False:
+    print("Please enter a numerical value.")
+    no_of_students = input("How many students will sit in the exam?: ")
 no_of_students = int(no_of_students)
 
 with open("reg_form.txt", "w", encoding = "utf-8") as students:
     for i in range(no_of_students):
         student_id = input("Enter the student's ID:")
-        student_id = int(student_id)
         students.write(f"{student_id}\n")
-        students.write("-" * 60)
+        students.write("-" * 80)
         students.write("\n")
